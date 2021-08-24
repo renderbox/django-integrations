15f62ece95447497264dc49a40e08ba2e1db8c53
Roberto Himmelbauer <robertoh89@gmail.com>
2021-08-24 11:57:51 -0700

    Created credentials model and initial migration

--- integrations/models.py
+++ integrations/models.py
@@ -1,22 +1,31 @@
-# import uuid 
-
-# from django.conf import settings
+from django.contrib.sites.models import Site
 from django.db import models
-# from django.utils.translation import ugettext as _
-# from django.urls import reverse
-# from django.contrib.sites.models import Site
+from django.utils.translation import ugettext_lazy as _
+from fernet_fields import EncryptedTextField
+def set_default_site_id():
+    return Site.objects.get_current()
 
+#######################################
+# ABSTRACT MODELS
+class CreateUpdateModelBase(models.Model):
+    '''
+    This is a shared models base that provides created & updated timestamp fields
+    '''
+    created = models.DateTimeField("date created", auto_now_add=True)
+    updated = models.DateTimeField("last updated", auto_now=True)
 
-# class SampleModel(models.Model):
-#     name = models.CharField(_("Name"), max_length=120, blank=False)
-#     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    class Meta:
+        abstract = True
 
-#     class Meta:
-#         verbose_name = _( "Sample Model")
-#         verbose_name_plural = _( "Sample Models")
-
-#     def __str__(self):
-#         return self.name
-
-#     def get_absolute_url(self):
-#         return reverse( "samplemodel_detail", kwargs={"uuid": self.uuid})
+class Credential(CreateUpdateModelBase):
+    """
+    Model to hold specified values for different integrations, the only required field is the site.
+    The logic behind the fields is left to the Integration Class defined by the develoepr
+    """
+    name = models.CharField(verbose_name=_("name"), blank=True, null=True, max_length=80)
+    site = models.ForeignKey(Site, verbose_name=_("Site"), on_delete=models.CASCADE, default=set_default_site_id, related_name="site")
+    client_id = models.CharField(verbose_name=_("Client ID"), blank=True, null=True, max_length=50)
+    client_url = models.CharField(verbose_name=_("Client URL"), blank=True, null=True, max_length=200)
+    public_key = EncryptedTextField(null=True, blank=True, verbose_name=_("Public Key"))
+    private_key = EncryptedTextField(null=True, blank=True, verbose_name=_("Private Key"))
+    attrs = models.JSONField(null=True, blank=True, default=dict)
