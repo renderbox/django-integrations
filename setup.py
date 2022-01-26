7ede975609b1e6e67dfc68e256df6e16e308fd1d
Roberto Himmelbauer <robertoh89@gmail.com>
2022-01-25 17:23:44 -0800

    removed condition for Django<2.0, replaced django-fernet-fields with djfernet that supports django 4

--- setup.py
+++ setup.py
@@ -45,8 +45,8 @@ setup(
     include_package_data=True,
     python_requires=">=3.6",
     install_requires=[
-        'Django>=3.0,<3.2',
-        'django-fernet-fields',
+        'Django>=3.0',
+        'djfernet',
     ],
     extras_require={
         'dev': [
